ac55c558e6b7bd63bda8feb56671e781af4c785f
mewfeuille115 <39928687+mewfeuille115@users.noreply.github.com>
2026-02-15 00:18:30 -0600

    feat(chapter 04): Duplicating Data Between Modules
    Introduces the Ticketing module, complete with customer functionalities.
    
    The user registration process is updated to create corresponding customer records in the Ticketing module using Domain Events instead of Synchronous Communication

--- src/Evently.Api/Program.cs
+++ src/Evently.Api/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddHealthChecks()
 
 builder.Services.AddEventsModule(builder.Configuration);
 builder.Services.AddUsersModule(builder.Configuration);
-builder.Services.AddTicketingModule(/*builder.Configuration*/);
+builder.Services.AddTicketingModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
